23ea9322e29364136c5f4238b3c5ed26706dc5a3
Piotr Pociask <piotr.pociask@gis-support.pl>
2017-02-20 19:08:07 +0100

    DB model

--- app.py
+++ app.py
@@ -2,15 +2,24 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from peewee import SqliteDatabase
+from peewee import OperationalError
+from models import db, Books, Magazines, Stories, Authors, Files
+import os
 
+#Initialize flask application
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_object('local_config')
 
-db = SqliteDatabase(app.config['DBPATH'])
-
-
+#Initialize databse
+db.init( app.config['DBPATH'] )
+if not os.path.exists( app.config['DBPATH'] ):
+    #Create db if not exist
+    try:
+        db.create_tables( [Books, Magazines, Stories, Authors, Files] )
+    except OperationalError:
+        pass
 
 if __name__ == '__main__':
+    #Run flask server
     app.run(host='0.0.0.0', port=6000)
--- models.py
+++ models.py
@@ -1,3 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from peewee import SqliteDatabase, Model, TextField, BlobField, IntegerField, \
+    ForeignKeyField
+from playhouse.sqlite_ext import JSONField
+
+db = SqliteDatabase(None)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Files(BaseModel):
+    filename = TextField()
+    content = BlobField()
+
+class Authors(BaseModel):
+    name = TextField()
+    surname = TextField()
+
+class Stories(BaseModel):
+    title = TextField()
+    author = ForeignKeyField(Authors, related_name='story_authors')
+
+class Magazines(BaseModel):
+    title = TextField()
+    number = IntegerField(null=True)
+    number_year = IntegerField(null=True)
+    year = IntegerField(null=True)
+    stories = ForeignKeyField(Stories, related_name='magazine_stories', null=True)
+    file_content = ForeignKeyField(Files, related_name='magazine_files', null=True)
+
+class Books(BaseModel):
+    title = TextField()
+    author = ForeignKeyField(Authors, related_name='books_authors', null=True)
+    file_content = ForeignKeyField(Files, related_name='book_files', null=True)
+    stories = ForeignKeyField(Stories, related_name='book_stories', null=True)
+    links = JSONField(null=True)
